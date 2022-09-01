a8dcfe6c19278cdd98716a9770873c59c407233d
Daniil Lashin <danil-lashin@yandex.ru>
2022-09-01 08:48:10 +0300

    Remove hardhat console

--- contracts/MinterLoans.sol
+++ contracts/MinterLoans.sol
@@ -6,8 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./Interfaces.sol";
 
-import "hardhat/console.sol";
-
 struct Loan {
     address borrower;
     address lender;
