806bf0d8b0d88b6e42de14fa456eea8eeeb76526
Daniil Lashin <danil-lashin@yandex.ru>
2022-09-01 13:01:11 +0300

    Enable optimizer

--- hardhat.config.js
+++ hardhat.config.js
@@ -24,7 +24,15 @@ if(process.env.BSC_PRIVATE_KEY) {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
   abiExporter: {
     path: './abi',
     runOnCompile: true
